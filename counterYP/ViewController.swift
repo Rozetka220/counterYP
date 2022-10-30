7726c789e9a2ab9eaae8b810e370fce1bd96b72c
Timur Bikbulatov <bikb.timur@yandex.ru>
2022-10-30 17:12:43 +0500

    Создан первичный рабочий счетчик согласно условиям ТЗ

--- counterYP/ViewController.swift
+++ counterYP/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBOutlet weak var countButton: UIButton!
+    private var count : Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        counterLabel.text = String(count)
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func addOneButton(_ sender: UIButton) {
+        count+=1
+        counterLabel.text = String(count)
+    }
+    
 }
 
